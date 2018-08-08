a75e26d986486b62d73a1061ec47cfac4c4383ab
puhongtao <pu.hongtao@yujianjia.com>
2018-08-08 16:28:08 +0800

    xx

---
+++ test/HelloWorld.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type HelloWorldChaincode struct {
+}
+
+func (t *HelloWorldChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("HelloWorld - Init called with function %s!\n", function)
+	return nil, nil
+}
+
+func (t *HelloWorldChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("HelloWorld - Invoke called with function %s!\n", function)
+
+	return nil, nil
+}
+
+func (t *HelloWorldChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("HelloWorld - Query called with function %s!\n", function)
+	message := "Hello World"
+	return []byte(message), nil
+}
